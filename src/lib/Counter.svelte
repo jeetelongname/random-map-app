56c4bc63f4bbc759ffaac71a17649ff257b25a13
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2023-03-25 11:22:58 +0000

    chore: initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+
+ let decCount = 0
+
+ const dec = () => {
+   decCount += -1
+ }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+
+<button on:click={dec}>
+  count is {decCount}
+</button>
